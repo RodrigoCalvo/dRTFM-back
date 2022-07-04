56293905946f4ae1c6d8d85ffc824be5344580d9
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-07-04 20:03:58 +0200

    Add document controller

--- src/document/document.controller.ts
+++ src/document/document.controller.ts
@@ -1,34 +1,50 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Post,
+    Body,
+    Patch,
+    Param,
+    Delete,
+} from '@nestjs/common';
 import { DocumentService } from './document.service';
 import { CreateDocumentDto } from './dto/create-document.dto';
 import { UpdateDocumentDto } from './dto/update-document.dto';
 
 @Controller('document')
 export class DocumentController {
-  constructor(private readonly documentService: DocumentService) {}
+    constructor(private readonly documentService: DocumentService) {}
 
-  @Post()
-  create(@Body() createDocumentDto: CreateDocumentDto) {
-    return this.documentService.create(createDocumentDto);
-  }
+    @Post()
+    create(@Body() createDocumentDto: CreateDocumentDto) {
+        return this.documentService.create(createDocumentDto);
+    }
 
-  @Get()
-  findAll() {
-    return this.documentService.findAll();
-  }
+    @Post(':id')
+    fork(@Param('id') id: string, @Body() idUser: string) {
+        return this.documentService.fork(id, idUser);
+    }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.documentService.findOne(+id);
-  }
+    @Get()
+    findAll() {
+        return this.documentService.findAll();
+    }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateDocumentDto: UpdateDocumentDto) {
-    return this.documentService.update(+id, updateDocumentDto);
-  }
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.documentService.findOne(id);
+    }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.documentService.remove(+id);
-  }
+    @Patch(':id')
+    update(
+        @Param('id') id: string,
+        @Body() updateDocumentDto: UpdateDocumentDto
+    ) {
+        return this.documentService.update(id, updateDocumentDto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.documentService.remove(id);
+    }
 }
