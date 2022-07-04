69ce16faf865ab359df7bf7bdd1e282353072812
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-07-04 20:03:14 +0200

    Fix bcryptjs service tests

--- src/auth/bcrypt.service.spec.ts
+++ src/auth/bcrypt.service.spec.ts
@@ -1,41 +1,24 @@
-import { Test, TestingModule } from '@nestjs/testing';
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { BcryptService } from './bcrypt.service';
-import bcryptjs from 'bcryptjs';
+import { hashSync, compareSync } from 'bcryptjs';
 
 jest.mock('bcryptjs');
+const mockEncrypt = jest.fn();
+const mockCompare = jest.fn();
+(hashSync as jest.Mock) = mockEncrypt;
+(compareSync as jest.Mock) = mockCompare;
 
 describe('BcryptService', () => {
-    let service: BcryptService;
-    const mockEncrypt = jest.fn();
-    const mockCompare = jest.fn();
-
-    beforeEach(async () => {
-        const module: TestingModule = await Test.createTestingModule({
-            providers: [BcryptService],
-        }).compile();
-
-        service = module.get<BcryptService>(BcryptService);
-    });
-
-    it('should be defined', () => {
-        expect(service).toBeDefined();
-    });
     describe('When service.encrypt method is called', () => {
-        test.todo(
-            'Then bcryptjs.hashSync should be called' //, () => {
-            //   bcryptjs.hashSync = mockEncrypt;
-            //   service.encrypt('');
-            //   expect(mockEncrypt).toHaveBeenCalled();
-            // }
-        );
+        test('Then bcryptjs.hashSync should be called', () => {
+            BcryptService.prototype.encrypt('');
+            expect(mockEncrypt).toHaveBeenCalled();
+        });
     });
     describe('When service.compare method is called', () => {
-        test.todo(
-            'Then bcryptjs.hashSync should be called' //, () => {
-            //   bcryptjs.compareSync = mockCompare;
-            //   service.compare('', '');
-            //   expect(mockCompare).toHaveBeenCalled();
-            // }
-        );
+        test('Then bcryptjs.hashSync should be called', () => {
+            BcryptService.prototype.compare('', '');
+            expect(mockCompare).toHaveBeenCalled();
+        });
     });
 });
