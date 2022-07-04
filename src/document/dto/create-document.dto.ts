69eb1c186954aa5835f4263156d50734a282957f
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-07-04 20:02:19 +0200

    Added dto document create

--- src/document/dto/create-document.dto.ts
+++ src/document/dto/create-document.dto.ts
@@ -1,1 +1,12 @@
-export class CreateDocumentDto {}
+import { Types } from 'mongoose';
+
+export class CreateDocumentDto {
+    title: string;
+    content: [
+        { text: string; options: { key: string; value: string | number } }
+    ];
+    keywords: [string];
+    author: Types.ObjectId;
+    fork?: Types.ObjectId;
+    visibility: 'public' | 'private';
+}
