23ae34773c36c864f4fdb7917da7cea824f99648
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-07-04 18:05:13 +0200

    Add user controller tests

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,20 +1,74 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { AuthService } from '../auth/auth.service';
+import { BcryptService } from '../auth/bcrypt.service';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 describe('UserController', () => {
-  let controller: UserController;
+    let controller: UserController;
+    let service: UserService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [UserController],
-      providers: [UserService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [UserController],
+            providers: [
+                {
+                    provide: UserService,
+                    useValue: {
+                        create: jest.fn(),
+                        login: jest.fn(),
+                        loginWithToken: jest.fn(),
+                    },
+                },
+                AuthService,
+                BcryptService,
+            ],
+        }).compile();
 
-    controller = module.get<UserController>(UserController);
-  });
+        controller = module.get<UserController>(UserController);
+        service = module.get<UserService>(UserService);
+    });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(controller).toBeDefined();
+    });
+
+    describe('When calling controller.create', () => {
+        test('Then service.create should be called', () => {
+            controller.create({
+                name: 'test',
+                email: 'test',
+                password: 'test',
+                myDocuments: [],
+                myFavs: [],
+            });
+            expect(service.create).toHaveBeenCalled();
+        });
+    });
+
+    describe('When calling controller.login without token', () => {
+        test('Then service.login should be called', () => {
+            controller.login(
+                {
+                    email: '',
+                    password: '',
+                },
+                undefined
+            );
+            expect(service.login).toHaveBeenCalled();
+        });
+    });
+
+    describe('When calling controller.login with token', () => {
+        test('Then service.loginWithToken should be called', () => {
+            controller.login(
+                {
+                    email: '',
+                    password: '',
+                },
+                'token'
+            );
+            expect(service.loginWithToken).toHaveBeenCalled();
+        });
+    });
 });
