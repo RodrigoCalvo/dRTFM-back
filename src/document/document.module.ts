ce714db248c69d023a3e40f27c90dafe50bc4e89
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-07-04 20:03:44 +0200

    Update document module imports

--- src/document/document.module.ts
+++ src/document/document.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { DocumentService } from './document.service';
 import { DocumentController } from './document.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { documentSchema } from './entities/document.entity';
+import { userSchema } from 'src/user/entities/user.entity';
 
 @Module({
-  controllers: [DocumentController],
-  providers: [DocumentService]
+    imports: [
+        MongooseModule.forFeature([
+            { name: 'Document', schema: documentSchema },
+            { name: 'User', schema: userSchema },
+        ]),
+    ],
+    controllers: [DocumentController],
+    providers: [DocumentService],
 })
 export class DocumentModule {}
