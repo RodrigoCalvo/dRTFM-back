a1a17cda98006440202875dc20c2d8f6da9d190f
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-07-06 14:12:52 +0200

    Add error control to login middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -4,17 +4,23 @@ import {
     UnauthorizedException,
 } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
+import { JwtPayload } from 'jsonwebtoken';
 import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
     constructor(private readonly auth: AuthService) {}
     use(req: Request, res: Response, next: NextFunction) {
-        const token = req.get('Authorization').substring(7); //token -bearer
-        const decodedToken = this.auth.decodeToken(token);
+        const token = req.get('Authorization');
+        if (!token) throw new UnauthorizedException("Token doesn't exist");
+        let decodedToken: string | JwtPayload;
+        try {
+            decodedToken = this.auth.decodeToken(token.substring(7));
+        } catch (e) {
+            throw new UnauthorizedException('Token expired');
+        }
         if (typeof decodedToken === 'string')
             throw new UnauthorizedException('Token expired');
         next();
     }
 }
-
