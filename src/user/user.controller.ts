242202ca619a91007d5901a74965df4923156d58
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-07-07 16:29:59 +0200

    Upgrade user controller w/ new user update

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -42,8 +42,11 @@ export class UserController {
     }
 
     @Patch(':id')
-    update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-        return this.userService.update(id, updateUserDto);
+    update(
+        @Headers('Authorization') token: string,
+        @Body() updateUserDto: UpdateUserDto
+    ) {
+        return this.userService.update(token, updateUserDto);
     }
 
     @Delete(':id')
