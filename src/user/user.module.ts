8dad7c8f8e3eefa84a0bb249b356d5e1a59a7101
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-07-08 11:09:01 +0200

    Add dependencies

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -6,10 +6,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { userSchema } from './entities/user.entity';
 import { AuthService } from '../auth/auth.service';
 import { BcryptService } from '../auth/bcrypt.service';
+import { documentSchema } from '../document/entities/document.entity';
 
 @Module({
     imports: [
-        MongooseModule.forFeature([{ name: 'User', schema: userSchema }]),
+        MongooseModule.forFeature([
+            { name: 'User', schema: userSchema },
+            { name: 'Document', schema: documentSchema },
+        ]),
     ],
     controllers: [UserController],
     providers: [UserService, AuthService, BcryptService],
