61d5fbe9f8e763638d3853395974d2917b1c35d0
Rodrigo Calvo <rodrigo.calvo.blazquez@gmail.com>
2022-07-18 21:29:35 +0200

    Update demo data

--- src/helpers/loadDB.ts
+++ src/helpers/loadDB.ts
@@ -10,9 +10,29 @@ export class LoadDB {
     myContent: Array<{ title: string; text: string; keywords: Array<string> }> =
         [
             {
-                title: '',
-                text: '',
-                keywords: [''],
+                title: 'Returns de los metodos de array',
+                text: 'find: el primer objeto que cumple la condicion, undefined si no hay ninguno\nmap: un array (igual de grande que el original) con la transformación de cada elemento a través de la callback\nfilter: un array con los elementos del original que cumplen la condicion de la callback, array vacío si ningun la cumple\nforEach: undefined siempre\npush: el tamaño del array después de insertar',
+                keywords: ['arrays', 'map', 'find', 'filter'],
+            },
+            {
+                title: 'useCallback',
+                text: 'useCallback sirve para que una función que no cambia no sea creada cada vez que se renderiza un componente de react\n\nDada una función definida en el componente y que está en un array de dependencias:\nconst prueba = () => { console.log("A fumar!") }\nUsamos useCallback para evitar que se genere una nueva cada vez:\nconst prueba = useCallback( () => { console.log("A fumar!") } )',
+                keywords: ['react', 'useCallback', 'hooks'],
+            },
+            {
+                title: 'Centrar un div',
+                text: 'Para centrar el contenido de un div en css, lo más fácil es usar un display:flex y luego un justify-content:center\nSi también queremos centrarlo en vertical porque el div tiene una altura marcada, añadimos align-items:center, y si hay varios elementos dentro del div y nos interesa que se muestren en columna en lugar de en fila, flex-direction:column',
+                keywords: ['css', 'centrar div'],
+            },
+            {
+                title: 'Anidar ternarios',
+                text: 'Primero de todo, anidar ternarios es, hablando en plata, de gilipollas\nPeeero en caso de necesidad imperiosa, ante por ejemplo una variable que puede tomar tres valores y que debe reflejarse en otros tres valores, el anidado de ternario sería el siguiente:\nlet emisor: true|false|undefined\nlet receptor: 1|2|3\n receptor = emisor === true ? 1 : emisor === false ? 2 : 3',
+                keywords: ['anidar ternarios', 'Alejandro'],
+            },
+            {
+                title: 'Formularios en react',
+                text: 'La clave de un formulario reactivo en React es guardar en un estado el contenido del input de texto en cualquier momento que cambie y mostrar en ese input en todo momento el contenido de nuestro estado\nconst [nombre, setNombre] = useState("")\nhandleChange(ev: SyntheticEvent) {\n\tconst eventTarget = ev.target as HTMLFormElement;\n\tsetFormData(eventTarget.value);\n\nY en el template:\n<input value={nombre} onChange={handleChange} />}',
+                keywords: ['react', 'formularios'],
             },
         ];
     myDocs: Array<iDocument> = [
